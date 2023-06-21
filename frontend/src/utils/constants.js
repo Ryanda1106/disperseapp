c1a4d336b007428be7c9fbf96d910f0617ed550c
Ryanda1106 <ryandakaminari@gmail.com>
2023-06-21 16:44:20 +0700

    pertama commit

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,40 @@
+export const supportedChains = [
+  {
+    chainId: 5,
+    disperseAddress: "0xD286f3D834E6030F178C395C9ba33d32B427cAD3",
+    blockExplorer: "https://goerli.etherscan.io/",
+    name: "goerli",
+  },
+  {
+    chainId: 11155111,
+    disperseAddress: "0xAf59E3d4aC260768A7803d67573d760bC508cCa9",
+    blockExplorer: "https://sepolia.etherscan.io/",
+    name: "sepolia",
+  },
+  {
+    chainId: 80001,
+    disperseAddress: "0xaf6f4A737188571d128477DEe81d54Bc63De6d80",
+    blockExplorer: "https://mumbai.polygonscan.com/",
+    name: "mumbai",
+  },
+  {
+    chainId: 278611351,
+    disperseAddress: "0x3bAB943FE31B0F90c1E4c2733113E336eBAC4Bc1",
+    blockExplorer:
+      "https://turbulent-unique-scheat.explorer.mainnet.skalenodes.com/",
+    name: "razor schain",
+  },
+  {
+    chainId: 137,
+    disperseAddress: "0x0461950C8664A38b6b1D6599420CA9c50a714B56",
+    blockExplorer: "https://polygonscan.com/",
+    name: "polygon",
+  },
+  {
+    chainId: 1517929550,
+    disperseAddress: "0x553a0a6FCe2943F6901f783f381292C567038ddF",
+    blockExplorer:
+      "https://staging-aware-chief-gianfar.explorer.staging-v3.skalenodes.com/",
+    name: "skale staging",
+  },
+];
